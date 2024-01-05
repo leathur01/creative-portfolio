aa791e2adc565b08b5e4f409a557b0c9d7f4c6b2
LeAthur01 <147360626+LeAthur01@users.noreply.github.com>
2024-01-05 18:27:39 +0700

    Implement data interactions and create constructor
    A constructor helps initilize an empty user object in the portfolio object

--- app/models/portfolio.go
+++ app/models/portfolio.go
@@ -1,19 +1,31 @@
 package models
 
 import (
+	"creative-portfolio/app"
+	"database/sql"
 	"fmt"
+	"time"
 
 	"github.com/revel/revel"
 )
 
 type Portfolio struct {
-	Id   int
-	Name string
-	User User
+	Id        int
+	Name      string
+	CreatedAt time.Time
+	User      *User
+}
+
+func NewPortfolio() Portfolio {
+	user := NewUser()
+
+	return Portfolio{
+		User: &user,
+	}
 }
 
 func (p *Portfolio) String() string {
-	return fmt.Sprintf("Portfolio(%s, %s)", p.Name, p.User.Name)
+	return fmt.Sprintf("Portfolio(%s, %d)", p.Name, p.User.Id)
 }
 
 func (p *Portfolio) Validate(v *revel.Validation) {
@@ -21,7 +33,190 @@ func (p *Portfolio) Validate(v *revel.Validation) {
 		revel.Required{},
 		revel.MaxSize{Max: 50},
 	)
+}
 
-	v.Check(p.User.Id,
-		revel.Required{})
+func InsertPortfolio(p Portfolio) error {
+	query := `
+		INSERT INTO portfolio(name, user_id) 
+		VALUES($1, $2);
+	`
+
+	args := []interface{}{p.Name, p.User.Id}
+	_, err := app.DB.Exec(query, args...)
+	return err
+}
+
+func GetPortfolio(id int) (*Portfolio, error) {
+	if id < 1 {
+		return nil, sql.ErrNoRows
+	}
+
+	query := `
+		SELECT id, name, created_at, user_id
+		FROM portfolio
+		WHERE id = $1;
+	`
+
+	args := []interface{}{id}
+	portfolio := NewPortfolio()
+	err := app.DB.QueryRow(query, args...).Scan(
+		&portfolio.Id,
+		&portfolio.Name,
+		&portfolio.CreatedAt,
+		&portfolio.User.Id,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &portfolio, nil
+}
+
+func GetPortfolioWithUser(id int) (*Portfolio, error) {
+	if id < 1 {
+		return nil, sql.ErrNoRows
+	}
+
+	query := `
+		SELECT p.id, p.name, p.created_at, u.id, u.name, u.email, u.created_at
+		FROM portfolio as p
+		JOIN "user" as u on p.user_id = u.id
+		WHERE p.id = $1;
+	`
+
+	args := []interface{}{id}
+	portfolio := Portfolio{}
+	user := NewUser()
+	err := app.DB.QueryRow(query, args...).Scan(
+		&portfolio.Id,
+		&portfolio.Name,
+		&portfolio.CreatedAt,
+		&user.Id,
+		&user.Name,
+		&user.Email,
+		&user.CreatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	portfolio.User = &user
+	user.Portfolios = append(user.Portfolios, &portfolio)
+
+	return &portfolio, nil
+}
+
+func GetAllPortfolios() ([]*Portfolio, error) {
+	query := `
+		SELECT id, name, created_at, user_id 
+		FROM portfolio
+	`
+
+	rows, err := app.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	portfolios := []*Portfolio{}
+	for rows.Next() {
+		portfolio := NewPortfolio()
+		err := rows.Scan(
+			&portfolio.Id,
+			&portfolio.Name,
+			&portfolio.CreatedAt,
+			&portfolio.User.Id,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		portfolios = append(portfolios, &portfolio)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return portfolios, nil
+}
+
+func GetAllPortfoliosOfUser(userId int) ([]*Portfolio, error) {
+	if userId < 1 {
+		return nil, sql.ErrNoRows
+	}
+
+	query := `
+		SELECT p.id, p.name, p.created_at, u.id, u.name, u.email, u.created_at
+		FROM portfolio as p
+		JOIN "user" as u on p.user_id = u.id
+		WHERE u.id = $1;
+	`
+
+	rows, err := app.DB.Query(query, userId)
+	if err != nil {
+		return nil, err //no rows
+	}
+
+	defer rows.Close()
+
+	user := NewUser()
+	portfolios := []*Portfolio{}
+	for rows.Next() {
+		portfolio := NewPortfolio()
+		rows.Scan(
+			&portfolio.Id,
+			&portfolio.Name,
+			&portfolio.CreatedAt,
+			&user.Id,
+			&user.Name,
+			&user.Email,
+			&user.CreatedAt,
+		)
+
+		if err != nil {
+			return nil, err //server error
+		}
+
+		portfolio.User = &user
+		portfolios = append(portfolios, &portfolio)
+	}
+
+	user.Portfolios = portfolios
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return portfolios, nil
+}
+
+func UpdatePortfolio(p Portfolio) error {
+	query := `
+		UPDATE portfolio
+		SET name = $1
+		WHERE id = $2;
+	`
+
+	args := []interface{}{p.Name, p.Id}
+	_, err := app.DB.Exec(query, args...)
+	return err
+}
+
+func DeletePortfolio(id int) error {
+	if id < 1 {
+		return sql.ErrNoRows
+	}
+
+	query := `
+		DELETE
+		FROM portfolio
+		WHERE id = $1;
+	`
+
+	_, err := app.DB.Exec(query, id)
+	return err
 }
