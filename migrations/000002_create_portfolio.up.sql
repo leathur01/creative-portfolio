53c76ab791261b9f37057e4568ff9ba02d9ad48f
LeAthur01 <147360626+LeAthur01@users.noreply.github.com>
2024-01-05 18:27:01 +0700

    Change portfolio database schema

--- migrations/000002_create_portfolio.up.sql
+++ migrations/000002_create_portfolio.up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS portfolio (
 	id serial PRIMARY KEY,
 	name text NOT NULL,
+	created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
 	user_id serial NOT NULL, 
 	CONSTRAINT fk_user
       FOREIGN KEY(user_id) 
