9168d3401f99263dbb815520914b2c634c877075
LeAthur01 <147360626+LeAthur01@users.noreply.github.com>
2024-01-05 18:31:42 +0700

    Build the app and prepare the test
    The test is prepared with basic use cases
    This simple uses cases help writing the test cases later

--- tests/apptest.go
+++ tests/apptest.go
@@ -1,6 +1,10 @@
 package tests
 
 import (
+	"creative-portfolio/app/models"
+	"fmt"
+
+	"github.com/revel/revel"
 	"github.com/revel/revel/testing"
 )
 
@@ -20,4 +24,60 @@ func (t *AppTest) TestThatIndexPageWorks() {
 
 func (t *AppTest) After() {
 	println("Tear down")
+}
+
+func (t *AppTest) InsertPortfolio() {
+	// Test insertintg portfolio belonging to user with id = 3
+	user := models.NewUser()
+	user.Id = 3
+
+	portfolio := models.NewPortfolio()
+	portfolio.Name = "media"
+	portfolio.User = &user
+
+	err := models.InsertPortfolio(portfolio)
+	if err != nil {
+		revel.AppLog.Fatal(err.Error())
+	}
+
+	revel.AppLog.Info("Insert successfully")
+
+}
+
+func (t *AppTest) GetPortfolio() {
+	// Test getting portfolio without a related user
+	portfolio, err := models.GetPortfolio(2)
+	if err != nil {
+		revel.AppLog.Fatal(err.Error())
+	}
+
+	revel.AppLog.Info(portfolio.String())
+
+	// Test getting portfolion with the related user
+	portfolio, err = models.GetPortfolioWithUser(2)
+	if err != nil {
+		revel.AppLog.Fatal(err.Error())
+	}
+
+	revel.AppLog.Info(portfolio.String())
+}
+
+func (t *AppTest) GetAllPortfolios() {
+	portfolios := []*models.Portfolio{}
+	portfolios, err := models.GetAllPortfolios()
+	if err != nil {
+		revel.AppLog.Fatal(err.Error())
+	}
+
+	revel.AppLog.Info(fmt.Sprintf(`%+v`, portfolios))
+}
+
+func (t *AppTest) GetAllPortfoliosOfUser() {
+	userId := 6
+	portfolios, err := models.GetAllPortfoliosOfUser(userId)
+	if err != nil {
+		revel.AppLog.Fatal(err.Error())
+	}
+
+	revel.AppLog.Info(fmt.Sprintf(`%+v`, portfolios))
 }
