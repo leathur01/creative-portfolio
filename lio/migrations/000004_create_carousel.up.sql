60e804c836f815359b335006534d35a4e52510c5
LeAthur01 <147360626+LeAthur01@users.noreply.github.com>
2024-03-16 14:28:43 +0700

    remove an unused field

--- lio/migrations/000004_create_carousel.up.sql
+++ lio/migrations/000004_create_carousel.up.sql
@@ -1,9 +1,8 @@
 CREATE TABLE carousel (
     id SERIAL PRIMARY KEY,
-    file_name varchar(255) NOT NULL,
     "order" int,
     file_path varchar(255) NOT NULL,
     file_size int NOT NULL,
     file_type varchar(50) NOT NULL,
-    uploaded_at timestamp(0) NOT NULL
+    uploaded_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
 );
